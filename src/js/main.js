32834ea0d79e9b38a0798b6a4623c48e268711b0
Adrian <adriannabozny15@gmail.com>
2023-08-30 16:21:47 +0200

    deleted scroll in element main, removed gap between contact and footer, added form validation

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,12 @@ let $mobileLinks;
 let $footerYear;
 let $nav;
 let $body;
+let $name;
+let $email;
+let $topic;
+let $message;
+let $sendMsgBtn;
+let $popup;
 
 const main = () => {
     prepareDOMElements();
@@ -19,6 +25,12 @@ const prepareDOMElements = () => {
     $footerYear = document.querySelector('.footer__year');
     $nav = document.querySelector('.nav');
     $body = document.querySelector('body');
+    $name = document.querySelector('#name');
+    $email = document.querySelector('#email');
+    $topic = document.querySelector('#topic');
+    $message = document.querySelector('#message');
+    $sendMsgBtn = document.querySelector('.contact__form-btn')
+    $popup = document.querySelector('.contact__popup')
 }
 
 const prepareDOMEvents = () => {
@@ -27,6 +39,15 @@ const prepareDOMEvents = () => {
     $burgerBtn.addEventListener('click', showNav)
     $mobileLinks.addEventListener('click', showNav)
     setPaddingScroll()
+    $sendMsgBtn.addEventListener('click', e => {
+        e.preventDefault();
+    
+        checkForm([$name, $email, $topic, $message])
+        checkLength($message, 5);
+        checkEmail($email);
+        checkErrors()
+    })
+    
 }
 
 const showNav = () => {
@@ -53,5 +74,61 @@ const setPaddingScroll = () => {
     $html.style.scrollPaddingTop = `${navHeight}px`
 }
 
-document.addEventListener('DOMContentLoaded', main);
+const showError = (input, msg) => {
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
 
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+}
+
+const checkForm = input => {
+    input.forEach(el => {
+        if (el.value === '') {
+            showError(el, `Pole ${el.placeholder} nie może być puste.`)
+        } else {
+            clearError(el)
+        };
+    });
+};
+
+const checkLength = (input, min) => {
+    if (input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} musi składać się z min. ${min} znaków.`);
+    }
+}
+
+const checkEmail = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(email.value)) {
+        clearError(email)
+    } else {
+        showError(email, 'E-mail jest niepoprawny')
+    }
+}
+
+const checkErrors = () => {
+
+    const allInputs = document.querySelectorAll('.contact__form-box');
+    let errorCount = 0;
+
+    allInputs.forEach(el => {
+        if (el.classList.contains('error')) {
+            errorCount++
+        }
+    })
+
+    if (errorCount === 0) {
+        $popup.classList.add('contact__show-popup')
+        $body.classList.add('disable-scroll')
+        
+    }
+}
+
+const clearError = (input) => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+}
+
+document.addEventListener('DOMContentLoaded', main);
